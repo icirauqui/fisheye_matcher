900e10f7072d7f86a23f778753e5cbd8a2235409
icirauqui <icirauqui@gmail.com>
2021-12-27 12:10:32 +0100

    geometry 1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,32 @@
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <iostream>
 
+// Function to find equation of plane.
+cv::Vec4f equation_plane(cv::Point3f p1, cv::Point3f p2, cv::Point3f p3){
+    float a1 = p2.x - p1.x;
+    float b1 = p2.y - p1.y;
+    float c1 = p2.z - p1.z;
+    float a2 = p3.x - p1.x;
+    float b2 = p3.y - p1.y;
+    float c2 = p3.z - p1.z;
+    float a = b1 * c2 - b2 * c1;
+    float b = a2 * c1 - a1 * c2;
+    float c = a1 * b2 - b1 * a2;
+    float d = (- a * p1.x - b * p1.y - c * p1.z);
+
+    cv::Vec4f piVec(a,b,c,d);
+    
+    return piVec;
+}
+
+void resize_and_display(const std::string& title, const cv::Mat& img1, float factor){
+    cv::Mat out1;
+    cv::resize(img1, out1, cv::Size(), factor, factor);
+
+    cv::imshow(title,out1);
+}
  
 static float distancePointLine(const cv::Point2f point, const cv::Vec3f& line)
 {
@@ -63,9 +88,53 @@ static void drawEpipolarLines(const std::string& title, const cv::Mat F,
       color);
     cv::circle(outImg(rect2), points2[i], 3, color, -1, cv::LINE_AA);
   }
-  cv::imshow(title, outImg);
+  //cv::imshow(title, outImg);
+  resize_and_display(title, outImg, 0.5);
   cv::waitKey(1);
 }
+
+static void drawEpipolarLines(const std::string& title, const cv::Mat F,
+                const cv::Mat& img1, const cv::Mat& img2,
+                const std::vector<cv::Point2f> points1,
+                const std::vector<cv::Vec3f> epilines1,
+                const std::vector<cv::Point2f> points2,
+                const float inlierDistance = -1) {
+  CV_Assert(img1.size() == img2.size() && img1.type() == img2.type());
+  cv::Mat outImg(img1.rows, img1.cols*2, CV_8UC3);
+  cv::Rect rect1(0,0, img1.cols, img1.rows);
+  cv::Rect rect2(img1.cols, 0, img1.cols, img1.rows);
+
+  if (img1.type() == CV_8U){
+    cv::cvtColor(img1, outImg(rect1), cv::COLOR_GRAY2BGR);
+    cv::cvtColor(img2, outImg(rect2), cv::COLOR_GRAY2BGR);
+  }
+  else{
+    img1.copyTo(outImg(rect1));
+    img2.copyTo(outImg(rect2));
+  }
+ 
+  //CV_Assert(points1.size() == points2.size() && points2.size() == epilines1.size() && epilines1.size() == epilines2.size());
+ 
+  cv::RNG rng(0);
+
+  for (size_t i=0; i<points2.size(); i++){
+    cv::circle(outImg(rect2), points2[i], 3, cv::Scalar(50,50,50), -1, cv::LINE_AA);
+  }
+
+  for(size_t i=0; i<points1.size(); i++) {
+    cv::Scalar color(rng(256),rng(256),rng(256));
+ 
+    cv::line(outImg(rect2),
+      cv::Point(0,-epilines1[i][2]/epilines1[i][1]),
+      cv::Point(img1.cols,-(epilines1[i][2]+epilines1[i][0]*img1.cols)/epilines1[i][1]),
+      color);
+    cv::circle(outImg(rect1), points1[i], 3, color, -1, cv::LINE_AA);
+  }
+
+  resize_and_display(title, outImg, 0.5);
+  cv::waitKey(1);
+}
+
 
 
 
@@ -115,7 +184,6 @@ int main(){
             good_matches.push_back(knn_matches[i][0]);
         }
     }
-
     std::vector<cv::Point2f> points1, points2;
     for (unsigned int i=0; i<good_matches.size(); i++){
         cv::Point2f pt1 = kps1[good_matches[i].queryIdx].pt;
@@ -128,10 +196,44 @@ int main(){
     // Epipolar matching
     // Compute F and epilines
     cv::Mat F12 = cv::findFundamentalMat(points1,points2);
-    cv::Mat F21 = cv::findFundamentalMat(points2,points1);
 
-    std::string title1 = "epip1";
-    drawEpipolarLines(title1,F12,im1,im2,points1,points2);
+    drawEpipolarLines("epip1",F12,im1,im2,points1,points2);
+
+
+
+
+
+    std::vector<cv::Point2f> kpoints1, kpoints2;
+    for (size_t i=0; i<kps1.size(); i++)
+      kpoints1.push_back(kps1[i].pt);
+    for (size_t i=0; i<kps2.size(); i++)
+      kpoints2.push_back(kps2[i].pt);
+
+    std::vector<cv::Vec3f> gmlines1, gmlines2;
+    cv::computeCorrespondEpilines(kpoints1, 1, F12, gmlines1);
+    cv::computeCorrespondEpilines(kpoints2, 2, F12, gmlines2);
+
+    drawEpipolarLines("epip2",F12,im1,im2,kpoints1,gmlines1,kpoints2);
+
+    cv::Point3f co2(cx,cy,f);
+
+    std::vector<cv::Point2f> gmpoints1, gmpoints2;
+    std::vector< std::vector<cv::DMatch> > gm_matches;
+    for (size_t i=0; i<gmlines1.size(); i++){
+      cv::Point3f pt0(0,-gmlines1[i][2]/gmlines1[i][1],0.);
+      cv::Point3f pt1(im1.cols,-(gmlines1[i][2]+gmlines1[i][0]*im1.cols)/gmlines1[i][1],0.);
+      cv::Vec4f pi = equation_plane(pt0, pt1, co2);
+
+      for (size_t j=0; j<kpoints2.size(); j++){
+        cv::Point3f kp(kpoints[j].x, kpoints2[j].y, 0.);
+        
+      }
+
+    }
+
+
+
+
 
     cv::waitKey(0);
     return 0;
